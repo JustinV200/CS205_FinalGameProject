46251660564bc69ec226aaf840d778d0cb628bf1
David Krumov <dkk003@bucknell.edu>
2024-11-11 15:26:53 -0500

    Test

--- src/main/java/org/StarWarsFinalProject/Movement.java
+++ src/main/java/org/StarWarsFinalProject/Movement.java
@@ -1,20 +1,24 @@
 /* *****************************************
-* CSCI 205 - Software Engineering and Design
-* Fall 2024
-* Instructor: Prof. Lily
-*
-* Name: David Krumov
-* Section: 9 AM
-* Date: 11/11/2024
-* Time: 3:19 PM
-*
-* Project: csci205_final_project
-* Package: org.StarWarsFinalProject
-* Class: Movement
-*
-* Description:
-*
-* ****************************************
-*/
-package org.StarWarsFinalProject;public class Movement {
+ * CSCI 205 - Software Engineering and Design
+ * Fall 2024
+ * Instructor: Prof. Lily
+ *
+ * Name: David Krumov
+ * Section: 9 AM
+ * Date: 11/11/2024
+ * Time: 3:19 PM
+ *
+ * Project: csci205_final_project
+ * Package: org.StarWarsFinalProject
+ * Class: Movement
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package org.StarWarsFinalProject;
+// Movement class for the characters
+
+public class Movement {
+
 }
