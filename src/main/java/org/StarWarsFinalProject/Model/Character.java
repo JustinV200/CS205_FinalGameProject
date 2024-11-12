a3166cf54249413808c1f58b847ff28289133faf
David Krumov <dkk003@bucknell.edu>
2024-11-11 20:57:07 -0500

    Basic Character Class Initialized

--- src/main/java/org/StarWarsFinalProject/Model/Character.java
+++ src/main/java/org/StarWarsFinalProject/Model/Character.java
@@ -18,5 +18,76 @@
  */
 package org.StarWarsFinalProject.Model;
 
+/**
+ * Program that creates each opposing character
+ */
 public class Character {
+
+    /** health of the character */
+    private double health;
+
+    /** name of the character */
+    private String name;
+
+    /** character's weapon */
+    private Weapon weapon;
+
+
+
+    /**
+     * Constructor for character class
+     *
+     * @param health - health of the character
+     * @param name - name of the character
+     * @param weapon - character's weapon
+     */
+    public Character(double health, String name, Weapon weapon) {
+        this.health = health;
+        this.name = name;
+        this.weapon = weapon;
+    }
+
+    /**
+     * Class for the character to take damage
+     *
+     * @param damage - the amount of damage inflicted from another opponent to the character
+     */
+    public void takeDamage(int damage) {
+        this.health -= damage;
+
+    }
+
+    /**
+     * Class to attack another Character object
+     *
+     * @param opponent - another character that will take damage
+     */
+    public void attack (Character opponent) {
+        int damage = this.weapon.getAttack();
+        opponent.takeDamage(damage);
+    }
+
+    /**
+     * Class that checks to see if character isAlive
+     *
+     * @return boolean on whether or not the character is alive or dead
+     */
+    public boolean isAlive() {
+        if (this.health < 0){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    /**
+     * Class that allows the character to take less damage by blocking
+     *
+     * @param damage - the amount of damage inflicted from another opponent to the character
+     */
+    public void block(int damage) {
+        double blockedDamage = damage * 0.5;
+        this.health -= blockedDamage;
+    }
 }
