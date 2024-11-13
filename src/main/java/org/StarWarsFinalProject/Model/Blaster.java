ce4b09884ec5e4d915c189d07982d87704a75db2
JustinVerlin <jav021@bucknell.edu>
2024-11-13 16:52:11 -0500

    updateBlaster
    made some small errors

--- src/main/java/org/StarWarsFinalProject/Model/Blaster.java
+++ src/main/java/org/StarWarsFinalProject/Model/Blaster.java
@@ -21,25 +21,25 @@ package org.StarWarsFinalProject.Model;
 public class Blaster extends Weapon {
     public double fireRate;
     ArrayList<Bullets> projectiles = new ArrayList<>();
-    public Blaster(damage, weaponType, fireRate) {
-        super(damage, weaponType)
+    public Blaster(double damage, String weaponType, double fireRate) {
+        super(damage, weaponType);
         this.fireRate = fireRate;
 
     }
     //takes in the location of the character, generates bullet at that location and fires
     public void shoot(int x, int y){
-        projectiles.add(new Bullets(x, y))
+        projectiles.add(new Bullets(x, y));
     }
     //clear the projectiles list of bullets
     public void clear(){
-        projectiles.clear()
+        projectiles.clear();
     }
     //first, move each bullet by its respective speed, currently set to 1, then, check for collisions with the enemy Character
     public boolean checkBullets(){
         for (bullet:projectiles){
             bullet.move(1);
             if(bullet.checkCollision() == true){
-                return true
+                return true;
             }
             //TODO, delete bullets
             //Psuedocode: if(bullet.x > certain number)
