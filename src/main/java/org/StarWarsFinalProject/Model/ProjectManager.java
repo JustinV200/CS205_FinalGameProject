78f7e3199d8d25eebfd453dbe0bc0e8c73f94213
David Krumov <dkk003@bucknell.edu>
2024-11-13 19:08:16 -0500

    Basic Model Classes

--- src/main/java/org/StarWarsFinalProject/Model/ProjectManager.java
+++ src/main/java/org/StarWarsFinalProject/Model/ProjectManager.java
@@ -18,5 +18,63 @@
  */
 package org.StarWarsFinalProject.Model;
 
+/**
+ * the Project Manager class, which manages all aspects of the game
+ * while it is being played by the users
+ */
 public class ProjectManager {
+
+    /** the number of rounds total*/
+    public int totalRounds;
+    /** the current round number*/
+    public int currentRound;
+    /** the left player */
+    public Character player1;
+    /** the right player */
+    public Character player2;
+    /** keeps track of the game time*/
+    public int timer;
+
+    /**
+     * constructor for the ProjectManager Class
+     */
+    public ProjectManager(int amountOfRounds) {
+        totalRounds = amountOfRounds;
+        currentRound = 1;
+        timer = 0;
+        player1 = new Character();
+        player2 = new Character();
+    }
+
+    /**
+     * a method which updates the current round the game is on
+     */
+    private void updateRound() {
+        currentRound++;
+    }
+
+    /**
+     *  a method which determine who is the winner after
+     *  a certain amount of rounds
+     */
+    private void determineWinner() {
+
+    }
+
+    /**
+     * updates the wins in each character class depending
+     * on who has one the round
+     */
+    public void updateWins() {
+
+    }
+
+    /**
+     * a method which determines if the game is over
+     *
+     * @return true if the game has finished, and false if not
+     */
+    private Boolean gameFinished() {
+        return false;
+    }
 }
